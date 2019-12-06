0e70c756191946f45642464cf2da4b387cdf0b9c
Kalkidanteferi <39376098+Kalkidanteferi@users.noreply.github.com>
2019-12-06 04:32:18 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
 
+<div class = "post_loop">
+
 <?php while (have_posts()) : the_post();?>
 
 <h2><?php the_title(); ?></h2>
@@ -8,6 +10,8 @@
 <P>by:<?php the_author(); ?><?php the_category(); ?></p>
 <P><?php the_content(); ?></p>
 
-<?php endwhile ?>
+<?php endwhile ?>>
+
+</div>
 
 <?php get_footer(); ?>
