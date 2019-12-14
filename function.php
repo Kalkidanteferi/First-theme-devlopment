bcec126918b0331c898f490e283a546abe7d2043
Kalkidanteferi <39376098+Kalkidanteferi@users.noreply.github.com>
2019-12-14 00:12:13 -0800

    Add files via upload

--- function.php
+++ function.php
@@ -8,6 +8,36 @@ function firsttheme_style(){
 add_action("wp_enqueue_scripts", "firsttheme_style" );
 
 
+if (function_exists('devwp_setup')):
+    function devwp_setup(){
+        load_theme_textdomain('devwp', get_template_directory() );
+        add_theme_support('automatic-feed-links');
+        add_theme_support('title-tag');
+        add_theme_support('post-thumbnails');
+
+        register_nav_menus(array(
+            'menu-1' => esc_html__('primary', 'devwp'),
+        ));
+        add_theme_support ('html5', array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+        ));
+        add_theme_support ('custome-background', apply_fillters('devwp_custome_background_args',
+        array(
+            'default-color' => 'ffffff',
+            'defauult-image' => '',
+        )));
+        add_theme_support ('customize-selective-refresh-widgets');
+        add_theme_support('custome-logo', array(
+            'height' => 400,
+            'width' => 400.
+            'flex-width' => true,
+            'flex-height' => true,
+        ));
+    }
+
 
 
 register_sidebar(array (
--- index.php
+++ index.php
@@ -3,10 +3,21 @@
 
 <div class = "post_loop">
 
+
 <?php while (have_posts()) : the_post();?>
 
 <h2><?php the_title(); ?></h2>
-
+<?php
+if(have_posts());
+if (is_home() && ! is_front_page()): ?>
+<header>
+    <h1 class = "page-title screen-reader-text"> <?php single_post_title(); ?></h1>
+</header>
+<?php
+endif;
+while (have_posts()) : the_post();
+    get_templat_pat('template-parts/content', get_post_format());
+endwhile;
 <P>by:<?php the_author(); ?><?php the_category(); ?></p>
 <P><?php the_content(); ?></p>
 
